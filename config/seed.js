9606dfaf60947d0639d4b4c5780920c0f8a596f7
徳永 優作 <yusaku_tokunaga@r.recruit.co.jp>
2015-07-07 01:04:38 +0900

    currentをNumberに変更

--- config/seed.js
+++ config/seed.js
@@ -18,52 +18,72 @@ User.find({}).remove(function() {
     name: '石部 達也',
     email: 'tatsuya_i7@r.recruit.co.jp',
     password: 'tatsuya_i7',
+    currentHaveToPay: 0,
+    currentPaid: 0
   }, {
     provider: 'local',
     name: '大迫 正和',
     email: 'masakazu_osako@r.recruit.co.jp',
     password: 'masakazu_osako',
+    currentHaveToPay: 0,
+    currentPaid: 0
   }, {
     provider: 'local',
     name: '奥野 悠一',
     email: 'yokuno0925@r.recruit.co.jp',
     password: 'yokuno0925',
+    currentHaveToPay: 0,
+    currentPaid: 0
   }, {
     provider: 'local',
     name: '木村 憲仁',
     email: 'knorihito@r.recruit.co.jp',
     password: 'knorihito',
+    currentHaveToPay: 0,
+    currentPaid: 0
   }, {
     provider: 'local',
     name: '越島 健介',
     email: 'koshijima@r.recruit.co.jp',
     password: 'koshijima',
+    currentHaveToPay: 0,
+    currentPaid: 0
   }, {
     provider: 'local',
     name: '晒谷 亮輔',
     email: 'ryosuke_sarashiya@r.recruit.co.jp',
     password: 'ryosuke_sarashiya',
+    currentHaveToPay: 0,
+    currentPaid: 0
   }, {
     provider: 'local',
     name: '徳永 優作',
     email: 'yusaku_tokunaga@r.recruit.co.jp',
     password: 'yusaku_tokunaga',
+    currentHaveToPay: 0,
+    currentPaid: 0
   }, {
     provider: 'local',
     role: 'admin',
     name: '朏島 一樹',
     email: 'haijima@r.recruit.co.jp',
     password: 'haijima',
+    currentHaveToPay: 0,
+    currentPaid: 0
   }, {
     provider: 'local',
     name: '本庄 智也',
     email: 't_honjo@r.recruit.co.jp',
     password: 't_honjo',
+    currentHaveToPay: 0,
+    currentPaid: 0
   }, {
     provider: 'local',
     name: '松山 勇輝',
     email: 'y_matsu@r.recruit.co.jp',
     password: 'y_matsu',
+    currentHaveToPay: 0,
+    currentPaid: 0
   }, function() {
     console.log('finished populating users');
 
