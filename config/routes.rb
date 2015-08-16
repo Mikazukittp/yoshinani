69778b349bdbc4315a19eba09c8456d9473a879d
haijima <k.haijima@gmail.com>
2015-08-16 18:38:10 +0900

    add auth feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     scope :v1 do
-      resources :users, controller: :user
+      resources :users, controller: :user do
+        collection do
+          post 'sign_in'
+        end
+      end
       resources :groups, controller: :group
       resources :payments, controller: :payment
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150813122807) do
+ActiveRecord::Schema.define(version: 20150814093833) do
 
   create_table "group_users", force: true do |t|
     t.integer  "group_id"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 20150813122807) do
     t.integer  "role",       default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "salt"
   end
 
 end
