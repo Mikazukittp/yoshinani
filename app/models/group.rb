64be3377d661558787f4ea541d8b2a9c9b52ec0f
matsumatsu20 <matsumatsu20@gmail.com>
2016-01-31 15:40:19 +0900

    テストの修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,10 +4,10 @@ class Group < ActiveRecord::Base
   has_many :payments
 
   def as_json(options={})
-    super methods: :include_users
+    super methods: :users_with_totals
   end
 
-  def include_users
+  def users_with_totals
     users.as_json(group_id: self.id)
   end
 end
