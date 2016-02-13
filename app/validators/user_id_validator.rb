5ad50c6851c3780091fd45eba1c17d56dda542eb
matsumatsu20 <matsumatsu20@gmail.com>
2016-02-14 00:37:30 +0900

    レコードがない場合にfindでエラーを吐いていた

--- app/validators/user_id_validator.rb
+++ app/validators/user_id_validator.rb
@@ -1,7 +1,9 @@
 class UserIdValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if value.present?
-      User.find(value).present?
+      return if User.exists?(id: value)
+
+      record.errors.add attribute, '指定されたuserは存在しません'
     end
   end
 end
