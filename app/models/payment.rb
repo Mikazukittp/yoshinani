6b84e32ecb6c022e0c140b9b3bee47c8db98e13b
matsumatsu20 <matsumatsu20@gmail.com>
2016-02-13 23:10:51 +0900

    :lipstick:

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -5,9 +5,7 @@ class Payment < ActiveRecord::Base
   has_many :participant_reference, class_name: 'Participant'
   has_many :participants, class_name: 'User', through: :participant_reference, source: :user
 
-  # validation
-  VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to:0 }
+  validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :event, presence: true, unless: :is_repayment
   validates :description, presence: true, unless: :is_repayment
   validates :date, presence: true
@@ -17,5 +15,4 @@ class Payment < ActiveRecord::Base
   def as_json(options={})
     super methods: [:paid_user, :participants]
   end
-
 end
