e65a15693b460dc8955287699d2a40d5585e2191
matsumatsu20 <matsumatsu20@gmail.com>
2016-02-14 15:48:46 +0900

    :lipstick:

--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -10,6 +10,7 @@ class GroupUser < ActiveRecord::Base
 
   def composite_primary_key
     relation = GroupUser.where(group_id: group.id, user_id: user.id)
+
     if relation.present?
       status = relation.first.status == :active ? "メンバー" : "招待中"
       errors[:base] << "すでに" + status + "のユーザがいます"
