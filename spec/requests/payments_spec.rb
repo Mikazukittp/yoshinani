59aa32757f325b601bb6397eeafd9ecffd9c107e
matsumatsu20 <matsumatsu20@gmail.com>
2016-02-17 00:22:55 +0900

    テスト書いた

--- spec/requests/payments_spec.rb
+++ spec/requests/payments_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe 'Users', type: :request do
     context '指定したグループに自分が所属している場合' do
       before do
         create(:group_user, user_id: sign_in_user.id, group_id: group.id)
+        create(:payment, event: '銀河一武道会', paid_user_id: user_1.id, group_id: group.id, deleted_at: Time.now)
 
         get api_payments_path, {group_id: group.id}, env
         @json = JSON.parse(response.body)
@@ -28,6 +29,10 @@ RSpec.describe 'Users', type: :request do
 
       example '期待したデータの一覧が取得されていること' do
         expect(@json[0]['event']).to eq '天下一武道会'
+      end
+
+      example '削除されたデータは取得されていないこと' do
+        expect(@json[1]).to be_nil
       end
     end
 
@@ -218,6 +223,50 @@ RSpec.describe 'Users', type: :request do
 
       before do
         patch api_payment_path(payment), {payment: {event: '銀河一武道会'}}, env
+        @json = JSON.parse(response.body)
+      end
+
+      example '403が返ってくること' do
+        expect(response).not_to be_success
+        expect(response.status).to eq 403
+      end
+    end
+  end
+
+  describe 'DELETE /api/payments/:id' do
+    let(:user_1) { create(:user, email: 'hunter1@example.com', account: 'hunter1') }
+    let(:user_2) { create(:user, email: 'hunter2@example.com', account: 'hunter2') }
+    let(:group) { create(:group) }
+
+    before do
+      create(:group_user, user_id: sign_in_user.id, group_id: group.id)
+      create(:group_user, user_id: user_1.id, group_id: group.id)
+      create(:group_user, user_id: user_2.id, group_id: group.id)
+    end
+
+    context '正しいパラメータを送った場合' do
+      let!(:payment) { create(:payment, paid_user_id: sign_in_user.id, group_id: group.id) }
+
+      before do
+        delete api_payment_path(payment), {}, env
+        @json = JSON.parse(response.body)
+      end
+
+      example '200が返ってくること' do
+        expect(response).to be_success
+        expect(response.status).to eq 200
+      end
+
+      example '期待したデータが取得されていること' do
+        expect(@json['deleted_at']).not_to be_nil
+      end
+    end
+
+    context '指定した精算のpiad_userが自分じゃない場合' do
+      let!(:payment) { create(:payment, paid_user_id: user_1.id, group_id: group.id) }
+
+      before do
+        delete api_payment_path(payment), {}, env
         @json = JSON.parse(response.body)
       end
 
