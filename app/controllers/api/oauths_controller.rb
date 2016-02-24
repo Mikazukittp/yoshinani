b46e0dd0df3657f0a22b9414ae33c36f2ed9010b
matsumatsu20 <matsumatsu20@gmail.com>
2016-02-24 22:42:51 +0900

    saltの処理（WIP）

--- app/controllers/api/oauths_controller.rb
+++ app/controllers/api/oauths_controller.rb
@@ -5,6 +5,8 @@ class Api::OauthsController < ApplicationController
 
     if exist_auth_registration?
       # ログイン
+      # validate_hash_token! saltや暗号化の形式が決まっていないためコメントアウト
+
       user = OauthRegistration.find_by(third_party_id: params[:oauth][:third_party_id], oauth_id: params[:oauth][:oauth_id]).user
       user.new_token
       user.save!
@@ -29,6 +31,16 @@ class Api::OauthsController < ApplicationController
   def deny_unpermitted_third_party
     unless Oauth.exists?(id: params[:oauth][:oauth_id])
       render json: {error: "許可されていないSNSです。"}, status: :bad_request
+      return
+    end
+  end
+
+  def validate_hash_token!
+    target_id = params[:oauth][:third_party_id]
+    salt = ENV["YOSHINANI_SALT"]
+
+    unless Digest::MD5.hexdigest(password + salt) == params[:oauth][:sns_hash_id]
+      render json: {error: "不正な操作です"}, status: :bad_request
       return
     end
   end
