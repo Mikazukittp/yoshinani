4935837b5722efa1e8cf1190a1603b9bdc44ba5d
matsumatsu20 <matsumatsu20@gmail.com>
2016-03-27 18:48:28 +0900

    エラーが発生してもloggerするだけにする

--- app/controllers/concerns/push_nortification.rb
+++ app/controllers/concerns/push_nortification.rb
@@ -4,22 +4,24 @@ module PushNortification
   def send_nortification(user, message)
     return unless user.nortification_tokens.present?
 
-    sns = Aws::SNS::Client.new(:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-                       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-                       :region => ENV['AWS_REGION'])
+    begin
+      sns = Aws::SNS::Client.new(:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+                         :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+                         :region => ENV['AWS_REGION'])
 
-    user.nortification_tokens.each do |nortification_token|
-      response = sns.create_platform_endpoint(
-                            platform_application_arn: ENV['AWS_SNS_APPLICATION_ARN'],
-                            token: nortification_token.device_token)
+      user.nortification_tokens.each do |nortification_token|
+        response = sns.create_platform_endpoint(
+                              platform_application_arn: ENV['AWS_SNS_APPLICATION_ARN'],
+                              token: nortification_token.device_token)
 
-      endpoint_arn = response[:endpoint_arn]
+        endpoint_arn = response[:endpoint_arn]
 
-      if sns.publish(target_arn: endpoint_arn, message: message, message_structure: 'json')
+        sns.publish(target_arn: endpoint_arn, message: message, message_structure: 'json')
         Rails.logger.info "publish success"
-      else
-        Rails.logger.error "publish failed"
       end
+
+    rescue => e
+      Rails.logger.error "publish to User #{user.id} failed #{e.try!(:message)}"
     end
   end
 end
