58f8d362e0af4c0e16122fe1e3fd65e5b27108f2
Yuki Matsuyama <matsumatsu20@gmail.com>
2016-04-03 19:57:19 +0900

    tokenごとにエラーキャッチをする

--- app/controllers/concerns/push_nortification.rb
+++ app/controllers/concerns/push_nortification.rb
@@ -10,32 +10,36 @@ module PushNortification
                          :region => ENV['AWS_REGION'])
 
       user.nortification_tokens.each do |nortification_token|
-        application_arn = nil
-        json_message = nil
+        begin
+          application_arn = nil
+          json_message = nil
 
-        case nortification_token.device_type
-        when 'android'
-          application_arn = ENV['GCM_SNS_APPLICATION_ARN']
+          case nortification_token.device_type
+          when 'android'
+            application_arn = ENV['GCM_SNS_APPLICATION_ARN']
 
-          orig_message = {data: {message: message, type: type}.merge(custom_data)}
-          json_message = JSON.generate({GCM: JSON.generate(orig_message)})
-        when 'ios'
-          application_arn = ENV['APNS_SNS_APPLICATION_ARN']
-          p application_arn
-          target_apns = Settings.aws.sns.target_apns.to_s
+            orig_message = {data: {message: message, type: type}.merge(custom_data)}
+            json_message = JSON.generate({GCM: JSON.generate(orig_message)})
+          when 'ios'
+            application_arn = ENV['APNS_SNS_APPLICATION_ARN']
+            p application_arn
+            target_apns = Settings.aws.sns.target_apns.to_s
 
-          orig_message = {aps: {alert: message, type: type}.merge(custom_data)}
-          json_message = JSON.generate({target_apns => JSON.generate(orig_message)})
+            orig_message = {aps: {alert: message, type: type}.merge(custom_data)}
+            json_message = JSON.generate({target_apns => JSON.generate(orig_message)})
+          end
+
+          response = sns.create_platform_endpoint(
+                                platform_application_arn: application_arn,
+                                token: nortification_token.device_token)
+
+          endpoint_arn = response[:endpoint_arn]
+
+          sns.publish(target_arn: endpoint_arn, message: json_message, message_structure: 'json')
+          Rails.logger.info "publish to User #{user.id} success"
+        rescue => e
+          Rails.logger.error "publish to User #{user.id} failed #{e.try!(:message)}"
         end
-
-        response = sns.create_platform_endpoint(
-                              platform_application_arn: application_arn,
-                              token: nortification_token.device_token)
-
-        endpoint_arn = response[:endpoint_arn]
-
-        sns.publish(target_arn: endpoint_arn, message: json_message, message_structure: 'json')
-        Rails.logger.info "publish success"
       end
 
     rescue => e
