cee4d0eaff50be43f501c4d00558cf148134f1d4
Yuki Matsuyama <matsumatsu20@gmail.com>
2016-04-18 16:39:53 +0900

    複合キーのユニーク制約を追加

--- app/models/oauth_registration.rb
+++ app/models/oauth_registration.rb
@@ -2,7 +2,7 @@ class OauthRegistration < ActiveRecord::Base
   belongs_to :user
   belongs_to :oauth
 
-  validates :user_id,  presence: true
+  validates :user_id,  presence: true, uniqueness: {scope: :oauth_id}
   validates :oauth_id, presence: true
   validates :third_party_id,  presence: true
 
